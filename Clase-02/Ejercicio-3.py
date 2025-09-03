319e81523d76ce56b83553aad8cb534e034446fa
Ignacio MIllapani <misttiski@gmail.com>
2025-09-03 12:34:04 -0400

    Ejercicios propuestos por el profesor, 3 completo, falta 1 y 2

---
+++ Clase-02/Ejercicio-3.py
@@ -0,0 +1,31 @@
+#Implementar una clase Fraccion que represente una fracción matemática con numerador y denominador. 
+class Fraccion:
+    def __init__(self, numerador, denominador):
+        self.numerador = numerador
+        self.denominador = denominador
+
+    #Además se debe crear varios métodos mágicos que permitan operar, comparar, y mostrar las fracciones de manera intuitiva. 
+    #La clase debe poseer los siguientes métodos mágicos:
+    #Método mágico que devuelva la fracción como una representación de cadena
+    def __str__(self): 
+        return f"La fraccion en cadena: {self.numerador}, {self.denominador}"
+    
+    #Método mágico que permita sumar dos fracciones
+    def __add__(self, fraccion):
+        return Fraccion(
+            self.numerador + fraccion.numerador,
+            self.denominador + fraccion.denominador
+        )
+    
+    #Método mágico que permita el producto entre dos fracciones
+    def __mul__(self, fraccion):
+        return Fraccion(
+            self.numerador * fraccion.numerador,
+            self.denominador * fraccion.denominador
+        )
+        
+    #Método mágico que permita comparar dos fracciones. Dos fracciones se consideran iguales si sus valores numéricos son equivalentes.
+    def __eq__(self, fraccion):
+        return {self.numerador, self.denominador} == {fraccion.numerador, fraccion.denominador}
+
+Fracciones = Fraccion #Se crea objeto
